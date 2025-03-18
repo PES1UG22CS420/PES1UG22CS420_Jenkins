56b4151705bd864dca05f303108092300f218670
PradhaanSBhat <127440295+PradhaanSBhat@users.noreply.github.com>
2025-03-18 15:54:54 +0530

    Commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void)
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Making a change to check whether Jenkins auto builds\n";
+  cout << "Making a change to test whether Jenkins auto builds\n";
   return 0;
 }
