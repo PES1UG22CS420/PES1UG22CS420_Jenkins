75f3a39e450246ac9c76682600084822330718e8
PradhaanSBhat <127440295+PradhaanSBhat@users.noreply.github.com>
2025-03-18 15:49:46 +0530

    Commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,11 +1,12 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Making a change to test whether Jenkins auto builds\n";
+  cout << "Making a change to check whether Jenkins auto builds\n";
   return 0;
 }
