3167c479e5155be7b1249f64815ef1956585e207
PradhaanSBhat <127440295+PradhaanSBhat@users.noreply.github.com>
2025-03-18 15:32:24 +0530

    Made change and test Jenkins

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Making a change to test whether Jenkins auto builds\n";
   return 0;
 }
